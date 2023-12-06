973a6e327d43098e4f640e186af2f0f88c293f9d
Jhon Arlex Ocampo <jhon.ocampo123@gmail.com>
2023-12-06 12:33:35 -0500

    Base de la app

---
+++ app-gestion-digital-facturas/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-gestion-digital-facturas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
